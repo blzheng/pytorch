59dcffc1fac2a397955499382765dc64c8d9d9a0
blzheng <beilei.zheng@intel.com>
2022-11-30 18:55:05 -0800

    ping benchmark to Nov_28

--- benchmarks/dynamo/torchbench.py
+++ benchmarks/dynamo/torchbench.py
@@ -119,7 +119,8 @@ REQUIRE_EVEN_HIGHER_TOLERANCE = {
 }
 
 REQUIRE_COSINE_TOLERACE = {
-    # Just keeping it here even though its empty, if we need this in future.
+    # https://github.com/pytorch/torchdynamo/issues/556
+    "resnet50_quantized_qat",
 }
 
 # non-deterministic output / cant check correctness
@@ -179,12 +180,6 @@ SKIP_ACCURACY_CHECK_MODELS = {
     "hf_GPT2_large",
     "hf_T5_large",
     "timm_vision_transformer_large",
-}
-
-
-MAX_BATCH_SIZE_FOR_ACCURACY_CHECK = {
-    "hf_GPT2": 2,
-    "pytorch_unet": 2,
 }
 
 
@@ -255,10 +250,6 @@ class TorchBenchmarkRunner(BenchmarkRunner):
         if batch_size is None and is_training and model_name in USE_SMALL_BATCH_SIZE:
             batch_size = USE_SMALL_BATCH_SIZE[model_name]
 
-        # Control the memory footprint for few models
-        if self.args.accuracy and model_name in MAX_BATCH_SIZE_FOR_ACCURACY_CHECK:
-            batch_size = min(batch_size, MAX_BATCH_SIZE_FOR_ACCURACY_CHECK[model_name])
-
         # workaround "RuntimeError: not allowed to set torch.backends.cudnn flags"
         torch.backends.__allow_nonbracketed_mutation_flag = True
         extra_args = []
@@ -301,6 +292,38 @@ class TorchBenchmarkRunner(BenchmarkRunner):
         # the right example_inputs
         if model_name == "yolov3":
             example_inputs = (torch.rand(batch_size, 3, 384, 512).to(device),)
+
+        if self.args.channels_last:
+            try:
+                model = model.to(memory_format=torch.channels_last).eval()
+                if isinstance(example_inputs, torch.Tensor) and example_inputs.dim()==4:
+                    example_inputs = example_inputs.to(memory_format=torch.channels_last)
+                elif isinstance(example_inputs, tuple):
+                    new_example_inputs = []
+                    for item in example_inputs:
+                        if isinstance(item, torch.Tensor) and item.dim()==4:
+                            new_example_inputs.append(item.to(memory_format=torch.channels_last))
+                        else:
+                            new_example_inputs.append(item)
+                    example_inputs = tuple(new_example_inputs)
+                elif isinstance(example_inputs, list):
+                    new_example_inputs = []
+                    for item in example_inputs:
+                        if isinstance(item, torch.Tensor) and item.dim()==4:
+                            new_example_inputs.append(item.to(memory_format=torch.channels_last))
+                        else:
+                            new_example_inputs.append(item)
+                    example_inputs = new_example_inputs
+                elif isinstance(example_inputs, dict):
+                    new_example_inputs = {}
+                    for k in example_inputs.keys():
+                        if isinstance(example_inputs[k], torch.Tensor) and example_inputs[k].dim() ==4:
+                            new_example_inputs[k] = example_inputs[k].to(memory_format=torch.channels_last)
+                        else:
+                            new_example_inputs[k] = example_inputs[k]
+                    example_inputs = new_example_inputs
+            except Exception:
+                pass
         # global current_name, current_device
         # current_device = device
         # current_name = benchmark.name
@@ -336,10 +359,9 @@ class TorchBenchmarkRunner(BenchmarkRunner):
         tolerance = 1e-4
         cosine = self.args.cosine
         # Increase the tolerance for torch allclose
-        if self.args.float16 or self.args.amp:
+        if self.args.float16:
             return 1e-3, cosine
         if is_training and current_device == "cuda":
-            tolerance = 1e-3
             if name in REQUIRE_COSINE_TOLERACE:
                 cosine = True
             elif name in REQUIRE_HIGHER_TOLERANCE:
