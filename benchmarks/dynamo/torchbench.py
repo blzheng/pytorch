06938aa086c269d572d50b191005b52bb6863272
blzheng <beilei.zheng@intel.com>
2022-12-21 18:11:08 -0800

    pin benchmarks to Dec_19

--- benchmarks/dynamo/torchbench.py
+++ benchmarks/dynamo/torchbench.py
@@ -280,7 +280,12 @@ class TorchBenchmarkRunner(BenchmarkRunner):
                 batch_size=batch_size,
                 extra_args=extra_args,
             )
-        model, example_inputs = benchmark.get_module()
+        if dynamic_shapes:
+            if not hasattr(benchmark, "get_dynamic_shapes_module"):
+                raise NotImplementedError("Dynamic Shapes not supported")
+            model, example_inputs = benchmark.get_dynamic_shapes_module()
+        else:
+            model, example_inputs = benchmark.get_module()
 
         # Models that must be in train mode while training
         if is_training and (not use_eval_mode or model_name in ONLY_TRAINING_MODE):
@@ -289,6 +294,8 @@ class TorchBenchmarkRunner(BenchmarkRunner):
             model.eval()
         gc.collect()
         batch_size = benchmark.batch_size
+
+        self.init_optimizer(device, model.parameters())
 
         # Torchbench has quite different setup for yolov3, so directly passing
         # the right example_inputs
